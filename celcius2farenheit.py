c469ae6c9118456cff14cd9ab9fab0bdf2164750
clavo24 <112191665+clavo24@users.noreply.github.com>
2022-09-12 22:23:57 -0600

    Add files via upload

---
+++ celcius2farenheit.py
@@ -0,0 +1,6 @@
+#Tendencias e Innovacion en Tecnologia Agricola
+tempCelcius = float(input("Cual es la temperatura en celcius? "))
+tempFarenheit = (tempCelcius * 9/5) + 32
+
+print("Temperatura en Farenheit = " + str(tempFarenheit))
+
---
+++ pagoporhora.py
@@ -0,0 +1,7 @@
+#Tendencias e Innovacion en Tecnologia Agricola
+numeroHoras = input("Cual es el numero de horas trabajadas? ")
+valorPorHoras = input("Cual es el valor por hora trabajada? ")
+
+totalPago = (int(numeroHoras) * int(valorPorHoras))
+print("Total a pagar = " + str(totalPago))
+
